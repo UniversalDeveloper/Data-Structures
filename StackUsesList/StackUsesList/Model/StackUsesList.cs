2f8aeb2f99ef2ff56a13e26f2b41c931f005210c
Anastasiia Lopata <lopata.ad@gmail.com>
2022-10-10 11:06:59 +0200

    created simple stack using list

---
+++ StackUsesList/StackUsesList/Model/StackUsesList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StackUsesList.Model
+{
+    class StackUsesList<T>
+    {
+        private List<T> dataStore = new List<T>();
+        private int countElInList => dataStore.Count;
+     
+        public T Pop()
+        {
+            if (countElInList>0)
+            {
+                var returnElFromList= dataStore.LastOrDefault();
+                dataStore.Remove(returnElFromList);
+                return returnElFromList;
+            }
+            else
+            {
+                throw  new NullReferenceException("Stack is empty");
+            }
+
+            
+        }
+
+
+        public void Push(T dataItem) 
+        {
+            dataStore.Add(dataItem);
+        }
+
+        public T Peek()
+        {
+            if (countElInList > 0)
+            {
+                return dataStore.LastOrDefault();
+            }
+            else
+            {
+                throw new NullReferenceException("Stack is empty");
+            }
+        }
+        public override string ToString()
+        {
+            return $"Stack with {countElInList} elements";
+        }
+    }
+}
---
+++ StackUsesList/StackUsesList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackUsesList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
